521ea7f8ca5b85049dd172cc9907d614bffc2c1d
syraxxxx <rsitraka181@gmail.com>
2023-01-17 02:12:46 +0300

    init template

--- src/app/home/pages/voiture/list-voiture/list-voiture.component.ts
+++ src/app/home/pages/voiture/list-voiture/list-voiture.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Voiture} from "../../../../@core/models/voiture.model";
 
 @Component({
   selector: 'app-list-voiture',
@@ -13,3 +14,9 @@ export class ListVoitureComponent implements OnInit {
   }
 
 }
+
+export const ELEMENT_DATA: Voiture[] = [
+  {_id:'1',idclient: '1', marque: 'Hydrogen1', model: "test", matricule: 'H',dans_garage:'yes'},
+  {_id:'2',idclient: '1', marque: 'Hydrogen2', model: "test", matricule: 'H',dans_garage:'yes'},
+  {_id:'3',idclient: '1', marque: 'Hydrogen3', model: "test", matricule: 'H',dans_garage:'yes'},
+];
