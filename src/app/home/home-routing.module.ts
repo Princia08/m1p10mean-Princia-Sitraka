bf478c1811fe7c82b1f77395495148131ce3ed34
syraxxxx <rsitraka181@gmail.com>
2023-01-17 17:58:40 +0300

    list voiture

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {AccueilComponent} from './pages/accueil/accueil.component';
 import {ListVoitureComponent} from "./pages/voiture/list-voiture/list-voiture.component";
 import {DetailVoitureComponent} from "./pages/voiture/detail-voiture/detail-voiture.component";
 import {HomeComponent} from "./home.component";
--- src/app/home/pages/voiture/list-voiture/list-voiture.component.ts
+++ src/app/home/pages/voiture/list-voiture/list-voiture.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Voiture} from "../../../../@core/models/voiture.model";
+import {MODEL_PROPERTIES_MAP, Voiture} from "../../../../@core/models/voiture.model";
 import {Router} from "@angular/router";
 
 @Component({
@@ -8,21 +8,25 @@ import {Router} from "@angular/router";
   styleUrls: ['./list-voiture.component.scss']
 })
 export class ListVoitureComponent implements OnInit {
+  titlesColumn = MODEL_PROPERTIES_MAP;
   voitures :Voiture[] = ELEMENT_DATA;
   voiture : Voiture | undefined;
+
   constructor(
     private router:Router
   ) { }
 
   ngOnInit(): void {
+    console.log(this.titlesColumn);
   }
   getDetail(voiture:Voiture){
     this.router.navigate(['/home/vehicule',voiture._id]);
   }
+
 }
 
 const ELEMENT_DATA: Voiture[] = [
-  {_id:'1',idclient: '1', marque: 'Hydrogen1', model: "test", matricule: 'H',dans_garage:'yes'},
-  {_id:'2',idclient: '1', marque: 'Hydrogen2', model: "test", matricule: 'H',dans_garage:'yes'},
-  {_id:'3',idclient: '1', marque: 'Hydrogen3', model: "test", matricule: 'H',dans_garage:'yes'},
+  {_id:'1',idclient: '1', marque: 'Citroen', model: "C4", matricule: '4565 TAB',dans_garage:'oui'},
+  {_id:'2',idclient: '1', marque: 'Ford', model: "Ranger", matricule: '4444 WWT',dans_garage:'oui'},
+  {_id:'3',idclient: '1', marque: 'Ford', model: "Raptor", matricule: '7899 TAH',dans_garage:'oui'},
 ];
