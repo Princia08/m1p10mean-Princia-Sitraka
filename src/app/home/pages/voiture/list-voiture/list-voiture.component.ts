f5659c089aab2e5043387023ad282e96cc444ca1
syraxxxx <rsitraka181@gmail.com>
2023-01-17 23:53:32 +0300

    create service voiture

--- src/app/home/pages/voiture/list-voiture/list-voiture.component.ts
+++ src/app/home/pages/voiture/list-voiture/list-voiture.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MODEL_PROPERTIES_MAP, Voiture} from "../../../../@core/models/voiture.model";
 import {Router} from "@angular/router";
+import {VoitureService} from "../../../../@core/services/voiture.service";
 
 @Component({
   selector: 'app-list-voiture',
@@ -9,24 +10,35 @@ import {Router} from "@angular/router";
 })
 export class ListVoitureComponent implements OnInit {
   titlesColumn = MODEL_PROPERTIES_MAP;
-  voitures :Voiture[] = ELEMENT_DATA;
-  voiture : Voiture | undefined;
+  voitures: Voiture[] | undefined;
+  voiture: Voiture | undefined;
 
   constructor(
-    private router:Router
-  ) { }
+    private router: Router,
+    private service: VoitureService
+  ) {
+  }
 
   ngOnInit(): void {
-    console.log(this.titlesColumn);
+    this.getData();
+    console.log("voiture here "+this.voitures)
   }
-  getDetail(voiture:Voiture){
-    this.router.navigate(['/home/vehicule',voiture._id]);
+
+  getData() {
+    this.service.getVoitures().subscribe(response => {
+      this.voitures = response;
+      console.log(response);
+    })
+  }
+
+  getDetail(voiture: Voiture) {
+    this.router.navigate(['/home/vehicule', voiture._id]);
   }
 
 }
 
 export const ELEMENT_DATA: Voiture[] = [
-  {_id:'1',idclient: '1', marque: 'Citroen', model: "C4", matricule: '4565 TAB',dans_garage:'oui'},
-  {_id:'2',idclient: '1', marque: 'Ford', model: "Ranger", matricule: '4444 WWT',dans_garage:'oui'},
-  {_id:'3',idclient: '1', marque: 'Ford', model: "Raptor", matricule: '7899 TAH',dans_garage:'oui'},
+  {_id: '1', idclient: '1', marque: 'Citroen', model: "C4", matricule: '4565 TAB', dans_garage: 'oui'},
+  {_id: '2', idclient: '1', marque: 'Ford', model: "Ranger", matricule: '4444 WWT', dans_garage: 'oui'},
+  {_id: '3', idclient: '1', marque: 'Ford', model: "Raptor", matricule: '7899 TAH', dans_garage: 'oui'},
 ];
