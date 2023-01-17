b42c2a92ca7d623822dde8768841f7a546bf90df
syraxxxx <rsitraka181@gmail.com>
2023-01-17 17:38:57 +0300

    test list and vehicles details

--- src/app/home/pages/voiture/list-voiture/list-voiture.component.ts
+++ src/app/home/pages/voiture/list-voiture/list-voiture.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Voiture} from "../../../../@core/models/voiture.model";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-list-voiture',
@@ -7,15 +8,20 @@ import {Voiture} from "../../../../@core/models/voiture.model";
   styleUrls: ['./list-voiture.component.scss']
 })
 export class ListVoitureComponent implements OnInit {
-
-  constructor() { }
+  voitures :Voiture[] = ELEMENT_DATA;
+  voiture : Voiture | undefined;
+  constructor(
+    private router:Router
+  ) { }
 
   ngOnInit(): void {
   }
-
+  getDetail(voiture:Voiture){
+    this.router.navigate(['/home/vehicule',voiture._id]);
+  }
 }
 
-export const ELEMENT_DATA: Voiture[] = [
+const ELEMENT_DATA: Voiture[] = [
   {_id:'1',idclient: '1', marque: 'Hydrogen1', model: "test", matricule: 'H',dans_garage:'yes'},
   {_id:'2',idclient: '1', marque: 'Hydrogen2', model: "test", matricule: 'H',dans_garage:'yes'},
   {_id:'3',idclient: '1', marque: 'Hydrogen3', model: "test", matricule: 'H',dans_garage:'yes'},
