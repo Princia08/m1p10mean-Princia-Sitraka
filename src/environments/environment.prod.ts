c78c559b40521822dac29bea7b42a67b118bfd45
unknown <80781644+Princia08@users.noreply.github.com>
2023-01-30 09:32:37 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  BASE: 'http://https://angular-deploy13.herokuapp.com/api',
+  BASE: 'https://angular-deploy13.herokuapp.com/api',
   directory: 'https://angular-deploy13.herokuapp.com',
 };
